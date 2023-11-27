5e49531aec94866ffdde4cd6c95147023af47fa1
Gustavo Prado <144186260+gustavoprado0@users.noreply.github.com>
2023-11-27 14:33:11 -0300

    Add files via upload

---
+++ Dia #13/index.js
@@ -0,0 +1,9 @@
+let pillot = confirm("Deseja que a decolagem seja efetuada capitão? Aguardando...")
+let ok = true;
+let cancel = false;
+
+if(pillot == ok){
+     console.log("Decolando!")
+}else if(pillot == cancel){
+     console.log("Decolagem cancelada!")
+}
